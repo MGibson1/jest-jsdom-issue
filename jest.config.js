e33c2a4a52b885402bc19801b17f32ed98a1c8db
Matt Gibson <mgibson@bitwarden.com>
2023-08-01 14:33:28 -0400

    Use minimal jsdom directly

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   preset: "ts-jest",
-  testEnvironment: "jsdom",
+  testEnvironment: "node",
   setupFilesAfterEnv: ["<rootDir>/test.setup.ts"],
 };
--- test.setup.ts
+++ test.setup.ts
@@ -1,4 +1,9 @@
 import { webcrypto } from "crypto";
+import { JSDOM } from "jsdom";
+
+Object.defineProperty(global, "window", {
+  value: new JSDOM("<!doctype html><html><body></body></html>"),
+});
 
 Object.defineProperty(window, "crypto", {
   value: webcrypto,

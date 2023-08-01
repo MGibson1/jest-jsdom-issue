46e771744ef54ff29165df9cee791264f512de40
Matt Gibson <mgibson@bitwarden.com>
2023-08-01 14:37:49 -0400

    remove duplicative tests

--- src/web-crypto-function.service.spec.ts
+++ src/web-crypto-function.service.spec.ts
@@ -10,29 +10,6 @@ describe("WebCryptoFunctionService", () => {
       const equal = await cryptoFunctionService.compare(a.buffer, a.buffer);
       expect(equal).toBe(true);
     });
-
-    it("should successfully compare two different values of the same length", async () => {
-      const cryptoFunctionService = getWebCryptoFunctionService();
-      const a = new Uint8Array(2);
-      a[0] = 1;
-      a[1] = 2;
-      const b = new Uint8Array(2);
-      b[0] = 3;
-      b[1] = 4;
-      const equal = await cryptoFunctionService.compare(a.buffer, b.buffer);
-      expect(equal).toBe(false);
-    });
-
-    it("should successfully compare two different values of different lengths", async () => {
-      const cryptoFunctionService = getWebCryptoFunctionService();
-      const a = new Uint8Array(2);
-      a[0] = 1;
-      a[1] = 2;
-      const b = new Uint8Array(2);
-      b[0] = 3;
-      const equal = await cryptoFunctionService.compare(a.buffer, b.buffer);
-      expect(equal).toBe(false);
-    });
   });
 });
 
